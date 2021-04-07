f6443d0fcb3defb23f744c859cc220820965f640
syh11026 <thsdbgus11026@naver.com>
2021-04-07 15:22:57 +0900

    added button

--- app/src/main/java/com/example/locproviderlist/LocProviderListActivity.java
+++ app/src/main/java/com/example/locproviderlist/LocProviderListActivity.java
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
 
-public class LocProviderListActivity extends AppCompatActivity {
+public class LocProviderListActivity extends AppCompatActivity{
     TextView mTextView;
     LocationManager locMgr;
     List<String> locProviders;
+    Button mbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +22,26 @@ public class LocProviderListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextView = findViewById(R.id.txtOutput);
-        locMgr = (LocationManager) getSystemService(LOCATION_SERVICE);
+        mbutton = findViewById(R.id.button);
+        mbutton.setOnClickListener(new View.OnClickListener() {
 
-        locProviders = locMgr.getAllProviders();
 
-        String s = "";
-        for(int i = 0 ; i<locProviders.size() ; i++){
-            s += "Loc. Provider : " + locProviders.get(i) + "\n"
-                    +"Status : " + locMgr.isProviderEnabled(locProviders.get(i)) +"\n\n";
+            @Override
+            public void onClick(View view) {
+
+                locMgr = (LocationManager) getSystemService(LOCATION_SERVICE);
+                locProviders = locMgr.getAllProviders();
+
+                String s = "";
+                for (int i = 0; i < locProviders.size(); i++) {
+                    s += "Loc. Provider : " + locProviders.get(i) + "\n"
+                            + "Status : " + locMgr.isProviderEnabled(locProviders.get(i)) + "\n\n";
+                }
+                mTextView.setText(s);
+            }
+
+        });
         }
-        mTextView.setText(s);
     }
-}
+
+
